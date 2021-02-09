1b21695d1f332f479800728ccd0b1b1547774e72
haro <daniel.kh555@gmail.com>
2021-02-09 01:59:41 +0100

    Glorious stereo-color asphalt

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+mod map;
+mod player;
+mod map_builder;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 90;
+    pub const SCREEN_HEIGHT: i32 = 60;
+    pub use crate::map::*;
+    pub use crate::player::*;
+    pub use crate::map_builder::*;
+}
+
+use prelude::*;
+
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            map: Map::new(),
+            player: Player::new(
+                Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+            ),
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::new()
+        .with_font("haro_16x16.png", 16, 16)
+        .with_tile_dimensions(16, 16)
+        .with_automatic_console_resize(true)
+        .with_simple_console(SCREEN_WIDTH, SCREEN_HEIGHT, "haro_16x16.png")
+        .with_title("Mt. Septune")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
+}
