962725176caab3ac7791c05a97844f7f560980f8
harofax <daniel.kh555@gmail.com>
2021-03-01 17:39:10 +0100

    small fix, upped fps cap

--- src/main.rs
+++ src/main.rs
@@ -195,7 +195,7 @@ impl GameState for State {
 fn main() -> BError {
     let context = BTermBuilder::new()
         .with_title("Mt. Septune")
-        .with_fps_cap(30.0)
+        .with_fps_cap(70.0)
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
         .with_tile_dimensions(16, 16)
         .with_resource_path("resources/")
